826456728643c9f5fa80a590c09f40c70b3a1bd5
B.D.H Madhuranga <mheshan27@gmail.com>
2022-01-27 13:45:11 +0530

    add default image

--- Server/models/Vehicle.js
+++ Server/models/Vehicle.js
@@ -7,6 +7,12 @@ const VehicleSchema = new mongoose.Schema(
     Price: { type: Number, required: true },
     Model: { type: String, required: true },
     YearofManufacture: { type: String, required: true },
+    img: {
+      type: String,
+      required: true,
+      default:
+        "https://firebasestorage.googleapis.com/v0/b/socialtest-cef88.appspot.com/o/Defaultimg%2Fundraw_Images_re_0kll.png?alt=media&token=ee57b9d5-fa04-4d85-8ac0-bd2cb8d04fb0",
+    },
     Mileage: { type: Number, required: true },
     Bid: [
       {
