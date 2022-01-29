30a7df8584b41f8eae4bc50431a3601a069b97d7
B.D.H Madhuranga <mheshan27@gmail.com>
2022-01-29 19:34:45 +0530

    update vehicle table

--- Client/src/component/Table/VehicleTable.js
+++ Client/src/component/Table/VehicleTable.js
@@ -13,28 +13,30 @@ export default function VehicleTable(props) {
     <div>
       <TableContainer>
         <Table aria-label="simple table">
-          <TableRow>
-            <TableCell align="left">Model</TableCell>
-            <TableCell align="left">{items.Model}</TableCell>
-          </TableRow>
-          {items.Brand && (
+          <TableBody>
             <TableRow>
-              <TableCell align="left">Brand</TableCell>
-              <TableCell align="left">{items.Brand}</TableCell>
+              <TableCell align="left">Model</TableCell>
+              <TableCell align="left">{items.Model}</TableCell>
             </TableRow>
-          )}
-          <TableRow>
-            <TableCell align="left">Mileage</TableCell>
-            <TableCell align="left">{items.Mileage}KM</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell align="left">Year of Manufacture</TableCell>
-            <TableCell align="left">{items.YearofManufacture}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell align="left">Price</TableCell>
-            <TableCell align="left">{items.Price}million</TableCell>
-          </TableRow>
+            {items.Brand && (
+              <TableRow>
+                <TableCell align="left">Brand</TableCell>
+                <TableCell align="left">{items.Brand}</TableCell>
+              </TableRow>
+            )}
+            <TableRow>
+              <TableCell align="left">Mileage</TableCell>
+              <TableCell align="left">{items.Mileage}KM</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="left">Year of Manufacture</TableCell>
+              <TableCell align="left">{items.YearofManufacture}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="left">Price</TableCell>
+              <TableCell align="left">{items.Price}million</TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
